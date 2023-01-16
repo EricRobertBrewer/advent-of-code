5193fdcb11c230cc113c50c877195515ecce7002
Eric Brewer <eric.r.brewer@gmail.com>
2023-01-16 12:26:37 -0700

    Move creation of year directory out of download routine.

--- python/aoc_util.py
+++ python/aoc_util.py
@@ -7,6 +7,7 @@ ENV_VAR = 'AOC_SESSION'
 
 def download_input_if_needed(year: str, day: int):
     year_dir = os.path.join(INPUT_DIR, year)
+    os.makedirs(year_dir, exist_ok=True)
     day_path = os.path.join(year_dir, '{:0>2d}.txt'.format(day))
     if not os.path.exists(day_path):
         if ENV_VAR not in os.environ:
@@ -15,7 +16,6 @@ def download_input_if_needed(year: str, day: int):
                            'export {}=<my_token>'.format(ENV_VAR, ENV_VAR))
         token = os.environ[ENV_VAR]
 
-        os.makedirs(year_dir, exist_ok=True)
         url = 'https://adventofcode.com/{}/day/{:d}/input'.format(year, day)
         req = request.Request(url, headers={'cookie': 'session={}'.format(token)})
         with request.urlopen(req) as fp:
