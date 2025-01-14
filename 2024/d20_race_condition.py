3d6a54d9d5172d7893fdbefd535847826b0cc197
Eric Brewer <eric.r.brewer@gmail.com>
2025-01-14 15:41:32 -0700

    Add day 20 part 2.

--- 2024/d20_race_condition.py
+++ 2024/d20_race_condition.py
@@ -19,52 +19,55 @@ def get_answer(lines, part):
     if p_end is None:
         raise ValueError('Unable to find end.')
 
-    p_to_cheat_to_d = dijkstra_breadth_first(grid, p_start, p_end)
+    duration_cheat = 2 if part == 1 else 20
+
+    p_to_d = dijkstra_grid(grid, p_start, None)
+    p_to_cheat_to_d = dict()
+    for p, d in p_to_d.items():
+        i, j = p
+        p_to_cheat_to_d[p] = dict()
+        for distance_cheat in range(2, duration_cheat + 1):
+            side_deltas = (zip(range(-distance_cheat, 1, 1), range(0, distance_cheat + 1, 1)),  # Top right.
+                           zip(range(-distance_cheat, 1, 1), range(0, -distance_cheat - 1, -1)),  # Top left.
+                           zip(range(distance_cheat, -1, -1), range(0, distance_cheat + 1, 1)),  # Bottom right.
+                           zip(range(distance_cheat, -1, -1), range(0, -distance_cheat - 1, -1)))  # Bottom left.
+            for deltas in side_deltas:
+                for di, dj in deltas:
+                    i_, j_ = i + di, j + dj
+                    if (0 <= i_ < len(grid) and 0 <= j_ < len(grid[i_]) and grid[i_][j_] != '#' and
+                            (i_, j_) in p_to_d.keys() and p_to_d[p] + distance_cheat < p_to_d[(i_, j_)]):
+                        p_to_cheat_to_d[p][(i_, j_)] = p_to_d[p] + distance_cheat
 
     answer = 0
-    cheat_to_d_end = p_to_cheat_to_d[p_end]
-    d_end_none = cheat_to_d_end[None]
-    for cheat in cheat_to_d_end.keys():
-        d = cheat_to_d_end[cheat]
-        if d_end_none - d >= 100:
-            answer += 1
+    for p, cheat_to_d in p_to_cheat_to_d.items():
+        for p_cheat, d_cheat in cheat_to_d.items():
+            if p_to_d[p_cheat] - d_cheat >= 100:
+                answer += 1
     return answer
 
 
-def dijkstra_breadth_first(grid, p_start, p_end):
+def dijkstra_grid(grid, p_start, p_end):
     deltas = ((-1, 0), (0, 1), (1, 0), (0, -1))
-
-    p_to_cheat_to_d = dict()
-    frontier = [(*p_start, None)]
+    p_to_d = dict()
+    frontier = [p_start]
     distance = 0
-    while len(frontier) > 0:
+    while len(frontier) > 0 and (p_end is None or p_end in p_to_d.keys()):
         frontier_next = list()
-        for i, j, cheat in frontier:
-            p = i, j
-            if p in p_to_cheat_to_d.keys():
-                if None in p_to_cheat_to_d[p].keys() or cheat in p_to_cheat_to_d[p].keys():
-                    # Don't bother back-tracking while carrying a cheat if you've already gotten there without one.
-                    continue
-            else:
-                p_to_cheat_to_d[p] = dict()
-            p_to_cheat_to_d[p][cheat] = distance
+        for p in frontier:
+            if p in p_to_d.keys():
+                continue
+            p_to_d[p] = distance
 
-            if p == p_end:
-                continue  # Don't explore from end.
-
+            i, j = p
             for di, dj in deltas:
                 i_, j_ = i + di, j + dj
-                if 0 <= i_ < len(grid) and 0 <= j_ < len(grid[i_]):
-                    if grid[i_][j_] == '#':
-                        if cheat is None:
-                            frontier_next.append((i_, j_, (i_, j_)))
-                    else:
-                        frontier_next.append((i_, j_, cheat))
+                if 0 <= i_ < len(grid) and 0 <= j_ < len(grid[i_]) and grid[i_][j_] != '#':
+                    frontier_next.append((i_, j_))
 
         frontier = frontier_next
         distance += 1
 
-    return p_to_cheat_to_d
+    return p_to_d
 
 
 if __name__ == '__main__':
