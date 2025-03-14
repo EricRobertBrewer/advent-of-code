8f73eeb77fe82c6da4c1051afb95c2a11c3fb073
Eric Brewer <eric.r.brewer@gmail.com>
2025-03-14 16:09:00 -0600

    Add day 3 part 2.

--- 2021/Day03_BinaryDiagnostic.hs
+++ 2021/Day03_BinaryDiagnostic.hs
@@ -13,11 +13,17 @@ getAnswer ss 1 =
         n = length ss
         gamma = toDecimal $ selectBits columnOneCounts n (>)
         epsilon = toDecimal $ selectBits columnOneCounts n (<)
+getAnswer ss 2 =
+    oxygen * co2
+    where
+        oxygen = toDecimal $ criteriaFilter ss (>=)
+        co2 = toDecimal $ criteriaFilter ss (<)
 
 oneCount :: String -> Int
-oneCount s =
-    sum $ map readC s
-    where readC c = read [c] :: Int
+oneCount s = sum $ map readC s
+
+readC :: Char -> Int
+readC c = read [c] :: Int
 
 selectBits :: [Int] -> Int -> (Int -> Int -> Bool) -> [Int]
 selectBits [] _ _ = []
@@ -27,3 +33,21 @@ toDecimal :: [Int] -> Int
 toDecimal xs =
     let digit = last xs
     in if null (init xs) then digit else 2 * toDecimal (init xs) + digit
+
+criteriaFilter :: [String] -> (Int -> Int -> Bool) -> [Int]
+criteriaFilter ss op =
+    case length ss of
+        1 -> map readC $ head ss
+        _ -> let (ss0, ss1) = binaryPartition ss
+             in if (op (length ss1) (length ss0))
+                then (1:criteriaFilter (map tail ss1) op)
+                else (0:criteriaFilter (map tail ss0) op)
+
+binaryPartition :: [String] -> ([String], [String])
+binaryPartition [] = ([], [])
+binaryPartition (s:ss) =
+    let (ss0, ss1) = binaryPartition ss
+    in case head s of
+       '0' -> ((s:ss0), ss1)
+       '1' -> (ss0, (s:ss1))
+       _ -> undefined
